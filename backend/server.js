79565655436e0113e4ecb42ec83ea4422a8d5f99
snehamn8 <snehamn88@gmail.com>
2025-08-13 21:25:43 +0530

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,21 @@ import postRoutes from "./routes/postRoutes.js";
 //import eventRegistrationRoutes from '../routes/eventRegistrationRoutes.js';
 import registrationRoutes from './routes/registrationRoutes.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
+
+
 dotenv.config();
 connectDB();
 
